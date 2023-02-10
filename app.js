afd1db1f76d14ce2749a5fd1b62a3942ba1c8b14
Shubham-02002 <shubhamgupta02002@gmail.com>
2023-02-10 20:18:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/",function(req,res){
+    var today = new Date();
+    
+    var options = {
+        weekday: "long",
+        day : "numeric",
+        month : "long"
+    }
+
+    var day = today.toLocaleDateString("en-us",options);
+    
+
+    
+    res.render("list",{kindOfDay:day, newListItem:'item'});
+})
+
+
+app.post("/", function(req,res){
+    var item = req.body.newItem
+    res.redirect('/')
+})
+
+
+app.listen(3000,function(req,res){
+    console.log('server is running on port 3000');
+})
